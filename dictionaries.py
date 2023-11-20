2930a40a668c5faee01775512b12695ec078de8a
Kevin <neoglitch822@yahoo.com>
2023-11-20 14:55:52 -0500

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -116,3 +116,36 @@ print(nums)
 print(2 in nums)
 
 # but you cannot refer to an element in the set with an index position or a key
+
+# Add a new element to a set
+nums.add(8)
+print(nums)
+
+# Add elements form one set to another
+morenums = {5,6,7}
+nums.update(morenums)
+print(nums)
+
+# you can use update with lists, tuples, and dictionaries too.
+
+# Merge two sets to create a new set
+one = {1,2,3}
+two = {5,6,7}
+
+mynewset = one.union(two)
+print(mynewset)
+
+# Keep only the duplicates
+one = {1,2,3}
+two = {2,3,4}
+
+one.intersection_update(two)
+print(one)
+
+# Keep everything EXCEPT the duplicates
+one = {1,2,3}
+two = {2,3,4}
+
+one.symmetric_difference_update(two)
+print(one)
+
