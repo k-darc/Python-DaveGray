8623d7834d8571ec67a4df5eb17a1d68c4cf4ea4
Kevin <neoglitch822@yahoo.com>
2023-11-20 15:56:45 -0500

    Update loops.py

--- loops.py
+++ loops.py
@@ -44,6 +44,10 @@ else:
 names = ["Dave", "Sara", "John"]
 actions = ["codes", "eats", "sleeps"]
 
-for name in names:
-    for action in actions:
+# for name in names:
+#     for action in actions:
+#         print(name + " " + action + ".")
+
+for action in actions:
+    for name in names:
         print(name + " " + action + ".")
