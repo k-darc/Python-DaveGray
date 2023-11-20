834161bfacf12e2ea865e6dcddbb5bce3e89facf
Kevin <neoglitch822@yahoo.com>
2023-11-20 14:39:41 -0500

    Update dictionaries.py
    good vs bad copy method

--- dictionaries.py
+++ dictionaries.py
@@ -3,3 +3,70 @@ band = {
     "vocals": "Plant",
     "guitar": "Page"
 }
+
+band2 = dict(vocals="Plant", guitar="Page") # With constructor function
+
+print(band)
+print(band2)
+print(type(band))
+print(len(band))
+
+# Access items
+print(band["vocals"])
+print(band.get("guitar"))
+
+# list all keys
+print(band.keys())
+
+# list all values
+print(band.values())
+
+# list of key.value pairs as tuples
+print(band.items())
+
+# verify a key exists
+print("guitar" in band)
+print("triangle" in band)
+
+# change values
+band["vocals"] = "Coverdale"
+band.update({"bass": "JPJ"})
+
+print(band)
+
+# Remove items
+print(band.pop("bass"))
+print(band)
+
+band["drums"] = "Bonham"
+print(band)
+
+print(band.popitem()) # tuple
+print(band)
+
+# Delete and clear
+
+band["drums"] = "Bonham"
+del band["drums"]
+print(band)
+
+band2.clear()
+print(band2)
+
+del band2
+
+# Copy dictionaries
+
+# band2 = band # creates a reference
+# print("Bad copy!")
+# print(band2)
+# print(band)
+
+# band2["drums"] = "Dave"
+# print(band)
+
+band2 = band.copy()
+band2["drums"] = "Dave"
+print("Good copy!")
+print(band)
+print(band2)
