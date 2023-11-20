dd683f13cb7a655a61151e2e71e381fd470a5bab
Kevin <neoglitch822@yahoo.com>
2023-11-20 15:53:10 -0500

    Update loops.py

--- loops.py
+++ loops.py
@@ -5,10 +5,37 @@ value = 1
 #         break
 #     value += 1
 
-while value <= 10:
-    value += 1
-    if value == 5:
-        continue
-    print(value)
-else:
-    print("Value is not equal to " + str(value))
+# while value <= 10:
+#     value += 1
+#     if value == 5:
+#         continue
+#     print(value)
+# else:
+#     print("Value is not equal to " + str(value))
+
+names = ["Dave", "Sara", "John"]
+# for x in names:
+#     print(x)
+
+# for x in "Mississippi":
+#     print(x)
+
+# for x in names:
+#     if x == "Sara":
+#         break
+#     print(x)
+
+# for x in names:
+#     if x == "Sara":
+#         continue
+#     print(x)
+
+# for x in range(4):
+#     print(x)
+
+# for x in range(2, 4):
+#     print(x)
+
+for x in range(0, 100, 5):
+    print(x)
+
