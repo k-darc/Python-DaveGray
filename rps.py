1db8926dbaa8822c70a7a96dab36ff93207fcb5e
Kevin <neoglitch822@yahoo.com>
2023-11-20 15:59:13 -0500

    Create rps.py

---
+++ rps.py
@@ -0,0 +1,31 @@
+import random
+
+options = ("rock", "paper", "scissors")
+playing = True
+
+while playing:
+
+    player = None
+    computer = random.choice(options)
+
+    while player not in options:
+        player = input("Enter a choice (rock, paper or scissors): ")
+
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
+        print(f"It's a tie no one has won.", player, "does not beat", computer)
+    elif player == "rock" and computer == "scissors":
+        print("You win!", player, "beats", computer)
+    elif player == "paper" and computer == "rock":
+        print(f"You win!", player, "beats", computer)
+    elif player == "scissors" and computer == "paper":
+        print(f"You win!", player, "beats", computer)
+    else:
+        print("You have lost", computer, "beats", player)
+
+    if not input("Play again? (y/n): ").lower() == "y":
+        player = False
+
+print("Thanks for player!")
