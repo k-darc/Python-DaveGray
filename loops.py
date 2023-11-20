43693543d2b178b1e0947be8a653e0faff293d7d
Kevin <neoglitch822@yahoo.com>
2023-11-20 15:28:38 -0500

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,4 +1,14 @@
 value = 1
-while value < 10:
-    print(value)
+# while value <= 10:
+#     print(value)
+#     if value == 5:
+#         break
+#     value += 1
+
+while value <= 10:
     value += 1
+    if value == 5:
+        continue
+    print(value)
+else:
+    print("Value is not equal to " + str(value))
