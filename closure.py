1b4ca5888af46e9b5ccc9a5d191dbb7ed443f6f3
Kevin <neoglitch822@yahoo.com>
2023-11-21 15:19:19 -0500

    Create closure.py

---
+++ closure.py
@@ -0,0 +1,18 @@
+# Closure is a function having access to the scope of its parent (nested) function after the parent function has returned.
+
+
+def parent_function(person):
+    coins = 3
+
+    def play_game():
+        nonlocal coins
+        coins -= 1
+
+        if coins > 1:
+            print("\n" + person + " has " + str(coins) + " coins left.")
+        elif coins == 1:
+            print("\n" + person + " has " + str(coins) + " coin left.")
+        else:
+            print("\n" + person + " is out of coins.")
+
+    return play_game
