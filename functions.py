0b8c1e90a2a65643f5a31628505e7f39c28887f5
Kevin <neoglitch822@yahoo.com>
2023-11-21 13:16:48 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,10 +1,14 @@
 def hello_world():
     print("Hello world!")
 
+
 hello_world()
 
+
 def sum(num1, num2):
+    if (type(num1) is not int or type(num2) is not int):
+        return
     return num1 + num2
 
-total = sum(2,3)
+total = sum("a",3)
 print(total)
