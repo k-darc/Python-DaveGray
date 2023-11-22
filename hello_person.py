0d0e4d62833af7d6a163a22c9050b90055afb9c3
Kevin <neoglitch822@yahoo.com>
2023-11-22 15:15:15 -0500

    Update hello_person.py

--- hello_person.py
+++ hello_person.py
@@ -1,14 +1,30 @@
-import argparse
+def hello(name, lang):
+    greetings = {
+        "English": "Hello"
+        "Spanish": "Hola"
+        "German": "Hallo"
+    }
+    msg = f"{greetings[lang]} {name}"
+    print(msg)
 
-parser = argparse.ArgumentParser(
-    description="Provides a personal greeting."
-)
+if __name__ == "__main__":
+    import argparse
 
-parser.add_argument(
-    "-n", "--name", metavar="name",
-    required=True, help="The name of the person to greet."
-)
+    parser = argparse.ArgumentParser(
+        description="Provides a personal greeting."
+    )
 
-args = parser.parse_args()
+    parser.add_argument(
+        "-n", "--name", metavar="name",
+        required=True, help="The name of the person to greet."
+    )
 
-msg = f"Hello {args.name}!"
+    parser.add_argument(
+        "-l", "--lang", metavar="language",
+        required=True, choices=["English", "Spanish", "German"]
+        help="The language of the greeting."
+    )
+
+    args = parser.parse_args()
+
+    hello(args.name, args.lang)
