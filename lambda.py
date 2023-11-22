c157e9a1defd977cc8409e298f4d5088706bdff6
Kevin <neoglitch822@yahoo.com>
2023-11-22 15:39:44 -0500

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,13 +1,24 @@
 def squared(num): return num * num
+# lambda num : num * num
 
 print(squared(2))
 
 addTwo = lambda num : num + 2
+# def addTwo(num): return num + 2 - this is the auto formatting for VSCode's opt in beta.
 
 print(addTwo(12))
-
-###########
 
 sum = lambda a, b : a + b
 
 print(sum(2, 2))
+
+#################################
+
+def funcBuilder(x):
+    return lambda num : num + x
+
+addTen = funcBuilder(10)
+addTwenty = funcBuilder(20)
+
+print(addTen(7))
+print(addTwenty(7))
