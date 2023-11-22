6a0a3329eafa0954f85a82b2550ccdafd9d9e5fd
Kevin <neoglitch822@yahoo.com>
2023-11-22 15:48:25 -0500

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -38,3 +38,13 @@ print(list(squared_nums))
 odd_nums = filter(lambda num : num % 2 != 0, numbers)
 
 print(list(odd_nums))
+
+###########################
+
+from functools import reduce
+
+numbers = [1,2,3,4,5,1]
+
+total = reduce(lambda acc, curr: acc + curr, numbers)
+
+print(total)
