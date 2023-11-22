7864271f3e63c7eadc28b34dd3c6b749e4d359fd
Kevin <neoglitch822@yahoo.com>
2023-11-22 15:02:33 -0500

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,3 +1,18 @@
-import math
 
-print(math.pi)
+
+from math import pi
+import sys
+import random as rdm
+from enum import Enum
+import kansas
+
+print(pi)
+
+# for item in dir(rdm):
+#     print(item)
+
+print(kansas.capital)
+kansas.randomfunfact()
+
+print(__name__)
+print(kansas.__name__)
