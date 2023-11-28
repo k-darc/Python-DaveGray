4026d6ca594b2d01ff5df11b887626b7a54689f6
Kevin <neoglitch822@yahoo.com>
2023-11-28 13:24:05 -0500

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -2,6 +2,10 @@ x = 2
 try:
     print(x / 0)
 except NameError:
-    print("NameError means something is probably undefined")
+    print("NameError means something is probably undefined.")
 except ZeroDivisionError:
-    print("Dividing by zero ends the universe")
+    print("Dividing by zero ends the universe. Don't do this.")
+else:
+    print("No errors!")
+finally:
+    print("I'm going to print with or without an error.")
