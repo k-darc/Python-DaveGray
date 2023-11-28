f38dea1a930988bbcc847700a27a67871f69ac91
Kevin <neoglitch822@yahoo.com>
2023-11-28 13:33:29 -0500

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,10 +1,14 @@
+class JustNotCoolError(Exception):
+    pass
 x = 2
 try:
-    print(x / 0)
+    raise NotCoolError("Not cool!!")
 except NameError:
     print("NameError means something is probably undefined.")
 except ZeroDivisionError:
     print("Dividing by zero ends the universe. Don't do this.")
+    # except: Exception as error:
+    #     print(error)
 else:
     print("No errors!")
 finally:
