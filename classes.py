42e34c8cff3f916f21e3f65df8b696bdaa38f22c
Kevin <neoglitch822@yahoo.com>
2023-11-28 12:03:28 -0500

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,7 +1,14 @@
 class Vehicle:
+    def __init__ (self, make, model):
+        self.make = make
+        self.model = model
+
     def moves(self):
         print("Moves along...")
 
-my_car = Vehicle()
 
+my_car = Vehicle("Tesla","Model 3")
+
+print(my_car.make)
+print(my_car.model)
 my_car.moves()
