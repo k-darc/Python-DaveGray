16940ce0fcacb4fc909901270f239419c701c3a6
Kevin <neoglitch822@yahoo.com>
2023-11-28 12:19:56 -0500

    Update classes.py

--- classes.py
+++ classes.py
@@ -20,3 +20,28 @@ my_car.moves()
 your_car = Vehicle("Cadillac", "Escalade")
 your_car.get_make_model()
 your_car.moves()
+
+
+class Airplane(Vehicle):
+    def __init__ (self, make, model, faa_id):
+        super(),__init__(make, model)
+        self.faa_id = faa_id
+
+class Truck(Vehicle):
+    def moves(self):
+        print("Rumbles along...")
+
+class GolfCart(Vehicle):
+    pass
+
+cessna = Airplane("Cessna","SkyHawk")
+
+mack = Truck("Mack","Pinnacle")
+golfwagon = GolfCart("Yamaha","GC100")
+
+cessna.get_make_model()
+cessna.moves()
+mack.get_make_model()
+mack.moves()
+golfwagon.get_make_model()
+golfwagon.moves()
