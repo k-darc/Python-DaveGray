11ce0b3f87938ee1fad56913c810df5f3c740827
Kevin <neoglitch822@yahoo.com>
2023-11-28 12:10:13 -0500

    Update classes.py

--- classes.py
+++ classes.py
@@ -16,3 +16,7 @@ my_car = Vehicle("Tesla","Model 3")
 # print(my_car.model)
 my_car.get_make_model()
 my_car.moves()
+
+your_car = Vehicle("Cadillac", "Escalade")
+your_car.get_make_model()
+your_car.moves()
